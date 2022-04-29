17e51a3da04578b5f7c1b62ae905a1f2edc2869a
eunalove <88129325+eunalove@users.noreply.github.com>
2022-04-29 16:00:31 +0900

    skyrotation

---
+++ metaBlock/Assets/Scripts/ControlSky.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSky : MonoBehaviour
+{
+
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * 0.5f);
+    }
+}
