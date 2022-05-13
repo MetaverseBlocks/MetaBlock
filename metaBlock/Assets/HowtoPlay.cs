453e9d6401e4ae7253fc48c48c202544daa160fe
yoogaeme <77279422+yoogaeme@users.noreply.github.com>
2022-05-13 17:46:21 +0900

    각종ui추가
    팝업창 해야함

---
+++ metaBlock/Assets/HowtoPlay.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HowtoPlay : MonoBehaviour
+{
+  public void ChangeSceneBtn()
+    {
+        switch (this.gameObject.name)
+        {
+            case "beforebtn":
+                SceneManager.LoadScene("MainMenu");
+                break;
+            case "startbtn":
+                SceneManager.LoadScene("SampleScene");
+                break;
+        }
+    }
+}
--- metaBlock/Assets/Scenes/ChangeScene.cs
+++ metaBlock/Assets/Scenes/ChangeScene.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class ChangeScene : MonoBehaviour
 {
+    public GameObject menuset;
     public void ChangeSceneBtn()
     {
         switch (this.gameObject.name)
@@ -12,6 +13,17 @@ public class ChangeScene : MonoBehaviour
             case "자유모드":
                 SceneManager.LoadScene("SampleScene");
                 break;
+            case "게임설명":
+                SceneManager.LoadScene("howtoplay");
+                break;
+            
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetButton("exitbtn"))
+        {
+            menuset.SetActive(true);
         }
     }
 }
